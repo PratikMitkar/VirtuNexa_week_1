c95c40fdafabe5dfab54ce93ede7c24482367708
Pratik Mitkar <56752342+PratikMitkar@users.noreply.github.com>
2025-01-12 13:33:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+document.getElementById('calculate_it').addEventListener('click',()=>{
+    const number = parseInt(document.getElementById('input_box').value);
+    if (!isValidNumber(number)) return;
+
+    const result = handle_calculate_it(number);
+    disply_result(number,result,"iterative")
+
+});
+document.getElementById('calculate_re').addEventListener('click',()=>{
+    const number = parseInt(document.getElementById('input_box').value);
+    
+    if (!isValidNumber(number)) return;
+
+    const result = handle_calculate_re(number);
+
+    disply_result(number,result,"recursive")
+
+});
+
+function isValidNumber(num) {
+    if (isNaN(num) || num < 0) {
+      alert("Please enter a valid positive integer.");
+      return false;
+    }
+    return true;
+  }
+
+
+function handle_calculate_it (n) {
+
+    let result = 1;
+
+    for (let i = 1; i <= n ; i++){
+        result *= i;    
+    }
+    return result;
+}
+
+function handle_calculate_re(n){
+    if(n === 0 || n === 1) return 1;
+
+    return n* handle_calculate_re(n-1);
+
+
+}
+
+function disply_result(number, result, method){
+    const result_div = document.getElementById('result');
+    result_div.innerHTML = `The factorial of "${number}".  <br/> The "${method}" method is:- ${result}`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger-menu");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
